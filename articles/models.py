0e1fcf9d7f873d68b7fb60f8e85b00f31a6495a1
seongjin <seongjin5743@naver.com>
2025-03-24 10:12:30 +0900

    modeling

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at =  models.DateTimeField(auto_now=False, auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+# class Comment(models.Model):
